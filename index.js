daad079d7e8dc7980e7a93d6a22288b56c9b8267
Suppa <joshsuppa@gmail.com>
2021-01-04 21:02:35 -0500

    added comments

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function add(a, b) {
 function log(message) {
   console.log("This is the message:", message);
 }
+
+// 1. Create new branch
+// 2. Add and commit a change to that branch
+// 3. Push the change to your remote branch (will be a new branch)
+// 4. Make a pull request for your new change into your master (or main, whatever)
+// 5. Sync up your local with your remote, then delete unneccessary branches
